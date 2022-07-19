bb32ae2f404fd755eb29ff156a7d31f99521b5d8
theikchan <theik.chan@wavemoney.com.mm>
2022-07-19 11:34:57 +0630

    Initial Commit

---
+++ UnitUIDemo/UnitUIDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitUIDemoApp.swift
+//  UnitUIDemo
+//
+//  Created by Theik Chan on 19/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct UnitUIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
